19a06f88dd478c74f03bf2304d9119ab420c776e
waikau maina <ericmaina.dev@gmail.com>
2022-11-13 00:18:56 +0300

    fixed the harsh

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require 'sinatra'
 set :default_content_type, 'application/json'
 get '/' do
   song = {
-    id: 1
-    name: "waikau"
+    "id" => 1
+    "name" => "waikau"
   }
   song.to_json
 end
